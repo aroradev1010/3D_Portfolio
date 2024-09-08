a99e6b14536fc668753f11e88d0bdfe79372d82c
Dev Arora <aroradev1010@gmail.com>
2024-09-08 13:14:26 +0530

    first commit

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,27 @@
+import { PerspectiveCamera } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import HackerRoom from "../components/HackerRoom";
+const Hero = () => {
+  return (
+    <section className="min-h-screen w-full flex flex-col relative" id="home">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+          Hi, I am Adrian <span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">
+          Building Products & Brands
+        </p>
+      </div>
+      <div className="w-full h-full absolute inset-0">
+        <Canvas className="w-full h-full">
+          <PerspectiveCamera makeDefault position={[0, 0, 30]}>
+            <HackerRoom />
+          </PerspectiveCamera>
+        </Canvas>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;

ac9a437b412bf40755f55cc01eeba0d2b3a4955c
Dev Arora <aroradev1010@gmail.com>
2024-09-08 18:29:49 +0530

    add hacker room 3d model to hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,54 @@
 import { PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React from "react";
+import React, { Suspense } from "react";
 import HackerRoom from "../components/HackerRoom";
+import CanvasLoader from "../components/CanvasLoader";
+import { Leva, useControls } from "leva";
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constants/index.js";
 const Hero = () => {
+  // const controls = useControls("HackerRoom", {
+  //   positionX: {
+  //     value: 0,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   positionY: {
+  //     value: 0,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   positionZ: {
+  //     value: 0,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   rotationX: {
+  //     value: 0,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   rotationY: {
+  //     value: 0,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   rotationZ: {
+  //     value: 0,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   scale: {
+  //     value: 0,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  // });
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
   return (
     <section className="min-h-screen w-full flex flex-col relative" id="home">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -14,10 +60,22 @@ const Hero = () => {
         </p>
       </div>
       <div className="w-full h-full absolute inset-0">
+        {/* <Leva /> */}
         <Canvas className="w-full h-full">
-          <PerspectiveCamera makeDefault position={[0, 0, 30]}>
-            <HackerRoom />
-          </PerspectiveCamera>
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 23]} />
+
+            <HackerRoom
+              scale={sizes.deskScale}
+              position={sizes.deskPosition}
+              rotation={[0.2, -Math.PI, 0]}
+            />
+            <group>
+              
+            </group>
+            <ambientLight intensity={1} />
+            <directionalLight intensity={0.5} position={[10, 10, 10]} />
+          </Suspense>
         </Canvas>
       </div>
     </section>

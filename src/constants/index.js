547b3fb43595e10f98c6c32328ecf536414067bf
Dev Arora <aroradev1010@gmail.com>
2025-06-10 13:39:01 +0530

    add new project

--- src/constants/index.js
+++ src/constants/index.js
@@ -42,6 +42,42 @@ export const clientReviews = [
 
 export const myProjects = [
   {
+    title: "Next.js Blog Platform - SEO-Optimized Modern Web App",
+    desc: "Infinite Ideas Hub is a dynamic blog website crafted with the power of Next.js. Dynamic blog application built with full-stack technologies for content publishing and user interaction.",
+    subdesc:
+      "This platform enables users to read and interact with blogs through features like liking, commenting, and exploring related posts. It includes a detailed blog view, author pages, and responsive UI with clean design. Built with Next.js, MongoDB, and Tailwind CSS, the app emphasizes performance, SEO, and modular code architecture.",
+    href: "https://infiniteideas-hub.vercel.app/",
+    texture: "/textures/project/project2.mp4",
+    logo: "/assets/project-logo3.png",
+    logoStyle: {
+      border: "0.2px solid #36201D",
+      backgroundColor: "black",
+    },
+    spotlight: "/assets/spotlight1.png",
+    tags: [
+      {
+        id: 1,
+        name: "Next.js",
+        path: "/assets/nextjs.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "NodeJs",
+        path: "assets/nodejs.png",
+      },
+      {
+        id: 4,
+        name: "MongoDB",
+        path: "assets/mongodb.svg",
+      },
+    ],
+  },
+  {
     title: "Tech Art Showcase - Immersive Portfolio with Interactive Elements",
     desc: "Introducing a vibrant and dynamic portfolio showcasing your creative work. The website's striking blue background, combined with captivating 3D elements, creates a visually stunning and immersive experience.",
     subdesc:
@@ -85,12 +121,50 @@ export const myProjects = [
     ],
   },
   {
-    title: "Infinite Ideas Hub - Blogging Platform",
+    title: "TeeCraft - Tshirt Designing Platform",
+    desc: "Tee Craft is a dynamic web application designed for creative minds who want to bring their T-shirt designs to life. With the power of React.js, Three.js, and Tailwind CSS, Tee Craft enables users to customize T-shirts effortlessly.",
+    subdesc:
+      "Change colors, upload files, and tweak the front logo design, all within an intuitive interface. Leveraging React for seamless interactivity, Three.js for engaging 3D visuals, and Tailwind CSS for a responsive design, Tee Craft is a testament to the fusion of cutting-edge technologies in web development. Dive into the world of personalized fashion with Tee Craft!.",
+    href: "https://thunderous-pegasus-3b214e.netlify.app/",
+    texture: "/textures/project/project1.mp4",
+    logo: "/assets/project-logo2.png",
+    logoStyle: {
+      backgroundColor: "black",
+
+      border: "0.2px solid #46753c",
+      boxShadow: "0px 0px 60px 0px #AA3C304D",
+    },
+    spotlight: "/assets/spotlight3.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+      {
+        id: 4,
+        name: "ThreeJS",
+        path: "/assets/project-logo2.png",
+      },
+    ],
+  },
+  {
+    title: "MERN Blog Platform - RESTful Full Stack App",
     desc: "Infinite Ideas Hub is a dynamic blog website crafted with the power of MERN (MongoDB, Express.js, React.js, Node.js) stack. Experience seamless user authentication, enabling users to create and share posts. Engage with the community through comments and likes on posts, fostering interaction and connection.",
     subdesc:
       "Admins enjoy a comprehensive dashboard to effortlessly manage posts, users, and comments, ensuring a smooth and organized blogging experience. Join our vibrant platform where ideas flourish and conversations thrive!",
     href: "https://infiniteideashub.onrender.com/",
-    texture: "/textures/project/project2.mp4",
+    texture: "/textures/project/project5.mp4",
     logo: "/assets/project-logo3.png",
     logoStyle: {
       border: "0.2px solid #36201D",
@@ -132,44 +206,6 @@ export const myProjects = [
         id: 7,
         name: "Redux",
         path: "assets/redux.png",
-      },
-    ],
-  },
-  {
-    title: "TeeCraft - Tshirt Designing Platform",
-    desc: "Tee Craft is a dynamic web application designed for creative minds who want to bring their T-shirt designs to life. With the power of React.js, Three.js, and Tailwind CSS, Tee Craft enables users to customize T-shirts effortlessly.",
-    subdesc:
-      "Change colors, upload files, and tweak the front logo design, all within an intuitive interface. Leveraging React for seamless interactivity, Three.js for engaging 3D visuals, and Tailwind CSS for a responsive design, Tee Craft is a testament to the fusion of cutting-edge technologies in web development. Dive into the world of personalized fashion with Tee Craft!.",
-    href: "https://thunderous-pegasus-3b214e.netlify.app/",
-    texture: "/textures/project/project1.mp4",
-    logo: "/assets/project-logo2.png",
-    logoStyle: {
-      backgroundColor: "black",
-
-      border: "0.2px solid #46753c",
-      boxShadow: "0px 0px 60px 0px #AA3C304D",
-    },
-    spotlight: "/assets/spotlight3.png",
-    tags: [
-      {
-        id: 1,
-        name: "React.js",
-        path: "/assets/react.svg",
-      },
-      {
-        id: 2,
-        name: "TailwindCSS",
-        path: "assets/tailwindcss.png",
-      },
-      {
-        id: 3,
-        name: "Framer Motion",
-        path: "/assets/framer.png",
-      },
-      {
-        id: 4,
-        name: "ThreeJS",
-        path: "/assets/project-logo2.png",
       },
     ],
   },
